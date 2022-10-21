6784f00654e5bdce56aacb082967b175033f8680
Ridhatama <ridhatama123@gmail.com>
2022-10-21 21:35:36 +0700

    Comments, Like
    Add comments, Hit like, Like Count

--- Foodgram/app/src/main/java/com/example/foodgram/LoginActivity.java
+++ Foodgram/app/src/main/java/com/example/foodgram/LoginActivity.java
@@ -11,7 +11,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.foodgram.Fragment.HomeFragment;
+import com.example.foodgram.Fragment.BottomNav;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
